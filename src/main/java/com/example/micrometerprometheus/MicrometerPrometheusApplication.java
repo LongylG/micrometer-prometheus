fb54d392bf407e4436fef5f31a5e2c1c5c8c0dbe
liyluong523@yahoo.com <liyulong523@yahoo.com>
2020-10-08 14:40:08 +0800

    first commit

---
+++ src/main/java/com/example/micrometerprometheus/MicrometerPrometheusApplication.java
@@ -0,0 +1,15 @@
+package com.example.micrometerprometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class MicrometerPrometheusApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(MicrometerPrometheusApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/example/micrometerprometheus/MicrometerPrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.micrometerprometheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrometerPrometheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
